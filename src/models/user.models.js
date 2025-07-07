e2cf5cda7dab2e9277f78411cab182d334163345
Dayamarkanthony <dayapal@markanthony.co.in>
2025-07-07 13:16:37 +0530

    add register controller

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,12 +1,9 @@
 
 import jwt from "jsonwebtoken";
 import bcrypt from 'bcrypt'
-import mongoose, { model, Schema } from "mongoose";
-
-
+import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-
     userName: {
         type: String,
         required: true,
@@ -64,7 +61,7 @@ const userSchema = new Schema({
 
 userSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next
-    this.password = bcrypt.hash(this.password, 10)
+    this.password =  await bcrypt.hash(this.password, 10)
     next()
 })
 
@@ -88,11 +85,11 @@ userSchema.methods.generateAccessToken = function () {
 
     )
 }
+
 userSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
-            _id: this._id,
-           
+            _id: this._id, 
         },
         process.env.REFRESH_TOKEN_SECRET,
         {
@@ -103,4 +100,4 @@ userSchema.methods.generateRefreshToken = function () {
 
 }
 
-export const User = model("User", userSchema)
+export const User = mongoose.model("User", userSchema)
